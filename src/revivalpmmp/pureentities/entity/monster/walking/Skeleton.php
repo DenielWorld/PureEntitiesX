422472a0b9b7949036c78b5f778791eee539adc9
DenielW <denielmirzoyan@gmail.com>
2019-02-15 18:36:14 -0800

    Update Skeleton.php

--- src/revivalpmmp/pureentities/entity/monster/walking/Skeleton.php
+++ src/revivalpmmp/pureentities/entity/monster/walking/Skeleton.php
@@ -18,155 +18,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+
 namespace revivalpmmp\pureentities\entity\monster\walking;
 
-use pocketmine\block\Water;
-use pocketmine\entity\Entity;
-use pocketmine\entity\projectile\Projectile;
-use pocketmine\entity\projectile\ProjectileSource;
-use pocketmine\event\entity\EntityShootBowEvent;
-use pocketmine\event\entity\ProjectileLaunchEvent;
-use pocketmine\item\Item;
-use pocketmine\item\ItemFactory;
-use pocketmine\level\Level;
-use pocketmine\level\sound\LaunchSound;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\DoubleTag;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\Player;
-use revivalpmmp\pureentities\components\MobEquipment;
 use revivalpmmp\pureentities\data\Data;
-use revivalpmmp\pureentities\entity\monster\WalkingMonster;
-use revivalpmmp\pureentities\features\IntfCanEquip;
 
 // use pocketmine\event\Timings;
 
-class Skeleton extends WalkingMonster implements IntfCanEquip, ProjectileSource{
-	const NETWORK_ID = Data::NETWORK_IDS["skeleton"];
-
-	/**
-	 * @var MobEquipment
-	 */
-	protected $mobEquipment;
-
-	protected $pickUpLoot = [];
-
-	public function initEntity() : void{
-		parent::initEntity();
-		$this->mobEquipment = new MobEquipment($this);
-		$this->mobEquipment->init();
-		if($this->mobEquipment->getMainHand() === null){
-			$this->equipDefaultMainHand();
-		}
-	}
-
-	protected function sendSpawnPacket(Player $player) : void {
-		parent::sendSpawnPacket($player);
-		$this->mobEquipment->sendEquipmentUpdate($player);
-	}
-
+class Skeleton extends Silverfish{
+	const NETWORK_ID = Data::NETWORK_IDS["stray"];
 	public function getName() : string{
-		return "Skeleton";
+		return "Magician";
+
 	}
 
-	/**
-	 * Attack the player
-	 *
-	 * @param Entity $player
-	 */
-	public function attackEntity(Entity $player){
-		if($this->attackDelay > 30 && mt_rand(1, 32) < 4 && $this->distanceSquared($player) <= 55){
-			$this->attackDelay = 0;
 
-			$f = 1.2;
-			$yaw = $this->yaw + mt_rand(-220, 220) / 10;
-			$pitch = $this->pitch + mt_rand(-120, 120) / 10;
-			$nbt = new CompoundTag("", [
-				"Pos" => new ListTag("Pos", [
-					new DoubleTag("", $this->x + (-sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5)),
-					new DoubleTag("", $this->y + 1.62),
-					new DoubleTag("", $this->z + (cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * 0.5))
-				]),
-				"Motion" => new ListTag("Motion", [
-					new DoubleTag("", -sin($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * $f),
-					new DoubleTag("", -sin($pitch / 180 * M_PI) * $f),
-					new DoubleTag("", cos($yaw / 180 * M_PI) * cos($pitch / 180 * M_PI) * $f)
-				]),
-				"Rotation" => new ListTag("Rotation", [
-					new FloatTag("", $yaw),
-					new FloatTag("", $pitch)
-				]),
-			]);
 
-			/** @var Projectile $arrow */
-			$arrow = Entity::createEntity("Arrow", $this->getLevel(), $nbt, $this);
-
-			$ev = new EntityShootBowEvent($this, Item::get(Item::ARROW, 0, 1), $arrow, $f);
-			$this->server->getPluginManager()->callEvent($ev);
-
-			$projectile = $ev->getProjectile();
-			if($ev->isCancelled()){
-				$projectile->kill();
-			}elseif($projectile instanceof Projectile){
-				$this->server->getPluginManager()->callEvent($launch = new ProjectileLaunchEvent($projectile));
-				if($launch->isCancelled()){
-					$projectile->kill();
-				}else{
-					$projectile->spawnToAll();
-					$this->level->addSound(new LaunchSound($this), $this->getViewers());
-				}
-			}
-
-			$this->checkTamedMobsAttack($player);
-		}
-	}
-
-	public function entityBaseTick(int $tickDiff = 1) : bool{
-		if($this->isClosed() or $this->getLevel() == null) return false;
-		// Timings::$timerEntityBaseTick->startTiming();
-
-		$hasUpdate = parent::entityBaseTick($tickDiff);
-
-		$time = $this->getLevel() ? $this->getLevel()->getTime() % Level::TIME_FULL : Level::TIME_NIGHT;
-		if(
-			!$this->isOnFire()
-			&& ($time < Level::TIME_NIGHT || $time > Level::TIME_SUNRISE)
-			&& !($this->getLevel()->getBlock($this) instanceof Water)
-		){
-			$this->setOnFire(100);
-		}
-
-		// Timings::$timerEntityBaseTick->stopTiming();
-		return $hasUpdate;
-	}
-
-	public function getDrops() : array{
-		$drops = [];
-		if($this->isLootDropAllowed()){
-			array_push($drops, Item::get(Item::ARROW, 0, mt_rand(0, 2)));
-			array_push($drops, Item::get(Item::BONE, 0, mt_rand(0, 2)));
-		}
-		return $drops;
-	}
-
-	public function getMaxHealth() : int{
-		return 20;
-	}
-
-	public function updateXpDropAmount() : void{
-		$this->xpDropAmount = 5;
-	}
-
-	public function getMobEquipment() : MobEquipment{
-		return $this->mobEquipment;
-	}
-
-	public function getPickupLoot() : array{
-		return $this->pickUpLoot;
-	}
-
-	protected function equipDefaultMainHand() : void{
-		$this->mobEquipment->setMainHand(ItemFactory::get(Item::BOW));
-	}
 }
+		
+
